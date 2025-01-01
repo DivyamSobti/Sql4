72c59c350611b63f0f0f0b1d472899a0aba7a024
Divyam Sobti <divyamsobti98@gmail.com>
2025-01-01 15:37:49 -0800

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,5 +1,26 @@
-SELECT 
-    player_id, event_date ,
-    SUM(games_played) 
-    OVER (PARTITION BY player_id order by event_date) AS games_played_so_far
-FROM Activity;
+# Write your MySQL query statement below
+with cte as 
+(
+select home_team_id as t, home_team_goals as goal, away_team_goals as away,
+case
+when home_team_goals > away_team_goals then 3
+when home_team_goals < away_team_goals then 0
+else 1
+end as points
+from Matches
+union all
+select away_team_id as t, away_team_goals as goal, home_team_goals as away,
+case
+when away_team_goals > home_team_goals then 3
+when away_team_goals < home_team_goals then 0
+else 1
+end as points
+from Matches
+)
+select t1.team_name, count(t1.team_id) as matches_played, sum(cte.points) as points,
+sum(cte.goal) goal_for,sum(cte.away)goal_against, sum(cte.goal) - sum(cte.away) as goal_diff
+from Teams t1
+Join cte
+on t1.team_id = cte.t
+group by t1.team_id
+order by points desc, goal_diff desc, team_name
