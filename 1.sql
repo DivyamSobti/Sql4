1389adfb3853836101f448521c727d1a275dfcbf
Divyam Sobti <divyamsobti98@gmail.com>
2025-01-01 15:47:10 -0800

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,11 +1,12 @@
-WITH cte AS (
-    SELECT 
-        player_id AS p, 
-        MIN(event_date) AS e
-    FROM Activity
-    GROUP BY player_id
+# Write your MySQL query statement below
+with cte as (
+    select employee_id, experience, sum(salary) over (partition by experience order by salary, employee_id) as ru_sum
+    from Candidates
 )
-SELECT a.player_id, a.device_id
-FROM Activity a
-JOIN cte 
-ON a.player_id = cte.p AND a.event_date = cte.e;
+Select 'Senior' as experience, count(employee_id) as accepted_candidates 
+from cte where experience ='senior' and ru_sum<=70000
+union
+Select 'Junior' as experience, count(employee_id) as accepted_candidates 
+from cte where experience ='junior' and ru_sum<=(
+Select 70000 - ifnull(max(ru_sum),0)
+from cte where experience ='senior' and ru_sum<=70000)
